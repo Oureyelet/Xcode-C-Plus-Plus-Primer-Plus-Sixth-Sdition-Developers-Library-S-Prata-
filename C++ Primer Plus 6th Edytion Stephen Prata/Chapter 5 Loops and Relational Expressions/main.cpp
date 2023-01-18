97402c6e4f67c39e5fc3a3dd41ed108cd36bdb22
Mateusz Kacperski <oureyelet@gmail.com>
2023-01-18 20:08:20 +0000

    stoped on exercise

--- C++ Primer Plus 6th Edytion Stephen Prata/Chapter 5 Loops and Relational Expressions/main.cpp
+++ C++ Primer Plus 6th Edytion Stephen Prata/Chapter 5 Loops and Relational Expressions/main.cpp
@@ -14,7 +14,7 @@
 #include <limits> // for: std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
 
-// Examples include in header files:
+// Examples has been included in header files:
 #include "Listing 5.1 forloop.h"
 #include "Listing 5.2 num_test.h"
 #include "Listing 5.3 express.h"
@@ -40,6 +40,9 @@
 void Programming_exercises_1();
 void Programming_exercises_2();
 void Programming_exercises_3();
+void Programming_exercises_4();
+double dephnes_invests(double x);     // Assistence prototype function fo Programming_exercises_4
+double cleos_invests(double x); // Assistence prototype function for Programming_exercises_4
 
 int main()
 {
@@ -727,7 +730,9 @@ int main()
     
     // Programming_exercises_1();
     // Programming_exercises_2();
-    Programming_exercises_3();
+    // Programming_exercises_3();
+    
+    Programming_exercises_4();
 
     std::cin.get();
     return 0;
@@ -803,4 +808,50 @@ void Programming_exercises_3()
         
         std::cout << "The sum of all the integers = " << answer << '\n';
     }
+}
+
+void Programming_exercises_4()
+{
+    /*
+     Daphne invests $100 at 10% simple interest.That is, every year, the investment earns 10% of the original investment, or $10 each and every year:
+     interest = 0.10 × original balance
+     
+     
+     At the same time, Cleo invests $100 at 5% compound interest.That is, interest is 5%
+     of the current balance, including previous additions of interest: interest = 0.05 × current balance
+     Cleo earns 5% of $100 the first year, giving her $105.The next year she earns 5% of $105, or $5.25, and so on.
+     
+     Write a program that finds how many years it takes for the value of Cleo’s investment to exceed the value of Daphne’s investment and then displays the value of both investments at that time.
+     */
+    
+    std::cout << "Enter you invest: ";
+    double invest;
+    std::cin >> invest;
+    double cleos = cleos_invests(invest);
+    double dephnes = dephnes_invests(invest);
+}
+
+double dephnes_invests(double x)
+{
+    double interest = 0.10 * x;
+
+    return interest;
+}
+
+double cleos_invests(double x)
+{
+    double current_balance = x;
+    // double interest = 0.05 * current_balance; // 105£
+    int year = 1;
+
+    while (year != 10)
+    {
+        current_balance = 0.05 * current_balance;
+        
+        std::cout << year << " year current balance = " << current_balance << '\n';
+        
+        year++;
+    }
+
+    return current_balance;
 }
