d256213f543b791d405e2aef51d2f1457ac70f2a
Mateusz Kacperski <oureyelet@gmail.com>
2023-01-28 21:56:19 +0000

    Chapter 6 ! lets do it ! :)

--- C++ Primer Plus 6th Edytion Stephen Prata/Chapter 6 Branching Statements and Logical Operators/main.cpp
+++ C++ Primer Plus 6th Edytion Stephen Prata/Chapter 6 Branching Statements and Logical Operators/main.cpp
@@ -1,5 +1,5 @@
 //
-//  main.cpp
+//  main.cpp -- S.Prata C++ Primer Plus 6th ed.
 //  Chapter 6 Branching Statements and Logical Operators
 //
 //  Created by Mateusz Kacperski on 28/01/2023.
@@ -7,8 +7,26 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World test !\n";
+int main()
+{
+    std::cout << "Hello, World!\n";
+    
+    /*
+     In this chapter you’ll learn about the following:
+     - The if statement
+     - The if else statement
+     - Logical operators: &&, ||, and !
+     - The cctype library of character functions n The conditional operator:?:
+     - The switch statement
+     - The continue and break statements
+     - Number-reading loops
+     - Basic file input/output
+     */
+    
+    //-----------------------------------------------------------------------------------------------------
+    // The if Statement: check example here -> "Listing 5.1 forloop.h"
+    //-----------------------------------------------------------------------------------------------------
+    
+    std::cin.get();
     return 0;
 }
