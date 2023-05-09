6095d2942421094cdf59b0f5a2604e8b34c121d5
Mateusz Kacperski <oureyelet@gmail.com>
2023-05-09 05:21:11 +0100

    Let's started 7th chapter. New folder has been created.

---
+++ C++ Primer Plus 6th Edytion Stephen Prata/Chapter 7 Functions C++'s Programming Module/Listing 7.1 calling.h
@@ -0,0 +1,19 @@
+//
+//  C++ Primer Plus 6th Edytion Stephen Prata
+//  Chapter 7 Functions C++'s Programming Modules
+//  Listing 7.1 -- defining, prototyping and calling a function
+//
+//  Created by Mateusz Kacperski on 9/05/2023.
+
+
+
+#ifndef Listing_7_1_calling_h
+#define Listing_7_1_calling_h
+
+void listing_7_1()
+{
+    
+}
+
+
+#endif /* Listing_7_1_calling_h */
---
+++ C++ Primer Plus 6th Edytion Stephen Prata/Chapter 7 Functions C++'s Programming Module/main.cpp
@@ -0,0 +1,59 @@
+//
+//  main.cpp -- C++ Primer Plus 6th Edytion Stephen Prata
+//  Chapter 7 Functions C++'s Programming Modules
+//
+//  Created by Mateusz Kacperski on 8/05/2023.
+
+
+
+#include <iostream>
+
+// Examples has been included in header files:
+#include "Listing 7.1 calling.h"
+
+// Function prototypes:
+bool is_alphabetick(char);
+void show_menu_exer_3();   // function menu for exercises 3
+void show_menu_exer_4();   // function menu for exercises 4
+bool begins_with_vowel(const std::string&);     // function check if first letter in word is vowel - for exercises 7
+ 
+// Function prototypes for 'Programin Exercises':
+void Programming_exercises_1();
+void Programming_exercises_2();
+void Programming_exercises_3();
+void Programming_exercises_4();
+void Programming_exercises_5();
+void Programming_exercises_6();
+void Programming_exercises_7();
+void Programming_exercises_8();
+void Programming_exercises_9();
+
+int main()
+{
+    std::cout << "Hello, World!\n";
+    
+    /*
+     In this chapter you’ll learn about the following:
+     - Function basics
+     - Function prototypes
+     - Passing function arguments by value
+     - Designing functions to process arrays
+     - Using const pointer parameters
+     - Designing functions to process text strings
+     - Designing functions to process structures
+     - Designing functions to process objects of the string class n Functions that call themselves (recursion)
+     - Pointers to functions
+     */
+    
+    
+    
+    
+    //-----------------------------------------------------------------------------------------------------
+    // Function Review: check example here -> "Listing 7.1 calling.h"
+    //-----------------------------------------------------------------------------------------------------
+    
+    // listing_6_1();
+    
+    
+    return 0;
+}
